c5dc2577ea7df399e5e2e0c6d3048f4dd6c649a3
jcatanop <jorgeenriquecatano@hotmail.com>
2025-06-17 21:25:57 -0400

    feat: add PII analysis function

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,37 @@ use pattern::core::PersonAnalyzer;
 use std::time::{Instant};
 use std::io;
 
-fn main() {
+fn detect_pii(text_to_analyze: &str) -> String {
+    let text_to_analyze = &text_to_analyze.trim();
 
-    println!("Please input your text to analyze:");
+    // Check if the text has any patterns like IP, credit card, US-SSN 
+    let registry = PatternRegistry::new();
+    let mut pattern = registry.detect_pattern(&text_to_analyze);
+    // If no PII was found, check if it could be PERSON
+    if pattern.is_empty() {
+        pattern = registry.detect_person_or_location("PERSON", &text_to_analyze);
+        
+        if pattern == "PERSON" {
+            let person_analyzer = PersonAnalyzer;
+            let analysis_result = person_analyzer.analysis(&text_to_analyze);
+            if !analysis_result.is_empty() {
+                pattern = analysis_result;
+            } else {
+                pattern = String::new();
+            }
+        }
+    }
+    // If no PII is detected yet, proceed to check if it is LOCATION
+    if pattern.is_empty() {
+        let text_without_space = &text_to_analyze.replace(" ", "");
+        pattern = registry.detect_person_or_location("LOCATION", &text_without_space);
+    }
+    pattern
+}
+
+fn main() {
     // Request a text to analyze
+    println!("\nPlease input your text to analyze:");
     let mut text_to_analyze = String::new();
     io::stdin()
     .read_line(&mut text_to_analyze)
@@ -15,35 +42,11 @@ fn main() {
     let start = Instant::now();
     println!("{}\n", "-".repeat(50));
 
-    let text_to_analyze: &str = text_to_analyze.trim();
-
-    // Check if the text has any patterns like IP, credit card, US-SSN 
-    let registry = PatternRegistry::new();
-    let mut pattern = registry.detect_pattern(text_to_analyze);
-
-    // If no PII was found, check if it could be PERSON
-    if pattern == "" {
-        pattern = registry.detect_person_or_location("PERSON", text_to_analyze);
-        
-        if pattern == "PERSON" {
-            let person_analyzer = PersonAnalyzer;
-            let analysis_result = person_analyzer.analysis(text_to_analyze);
-            if !analysis_result.is_empty() {
-                pattern = analysis_result;
-            } else {
-                pattern = String::new();
-            }
-        }
-    }
-
-    // If no PII is detected yet, proceed to check if it is LOCATION
-    if pattern == "" {
-        let text_without_space = &text_to_analyze.replace(" ","");
-        pattern = registry.detect_person_or_location("LOCATION", &text_without_space);
-    }
+    // Start PII analysis
+    let pii = detect_pii(&text_to_analyze);
 
     // Show result
-    println!("PII Type: {} \nText analyzed: {}", pattern, text_to_analyze);
+    println!("PII Type: {} \nText analyzed: {}", pii, text_to_analyze);
     let duration = start.elapsed();
     println!("Time taken: {} ms", duration.as_millis());
 }
